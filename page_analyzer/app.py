af4bf4f767d413836090f280ee7e09e1d4abfd54
Rustem <rustemkago12345@gmail.com>
2025-05-30 10:40:26 +0300

    добавлен секрет для подписи форм

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,11 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
 
+load_dotenv()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 @app.route("/")
