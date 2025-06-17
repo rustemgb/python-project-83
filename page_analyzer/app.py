90bad0b5189192f3217b29216b989f6a502e9469
Rustem <rustemkago12345@gmail.com>
2025-06-17 15:36:51 +0300

    add create url

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,9 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+from page_analyzer.data_base import save_url
+
 
 load_dotenv()
 app = Flask(__name__)
@@ -11,3 +13,11 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 @app.route("/", methods=["GET"])
 def index():
     return render_template('index.html')
+
+
+@app.route("/urls", methods=["POST"])
+def create_url():
+    input_url = request.form.get("url")
+    url_id = save_url(input_url)
+    flash('Страница успешно добавлена', 'success')
+    return render_template('index.html', url=url_id)
