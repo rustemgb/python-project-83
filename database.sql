6111d360c52eba2145914bf87d737d23fa3d2878
Rustem <rustemkago12345@gmail.com>
2025-06-17 15:04:35 +0300

    add database

---
+++ database.sql
@@ -0,0 +1,13 @@
+CREATE TABLE urls (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    created_at DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER NOT NULL REFERENCES urls(id),
+    code_status INTEGER,
+    description TEXT,
+    created_at DATE DEFAULT CURRENT_DATE
+);
