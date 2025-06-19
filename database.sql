e72e4c669ef52bcc2cd21cbc1468d601d740fa4f
Rustem <rustemkago12345@gmail.com>
2025-06-19 09:17:08 +0300

    add t1, title

--- database.sql
+++ database.sql
@@ -7,7 +7,9 @@ CREATE TABLE urls (
 CREATE TABLE url_checks (
     id SERIAL PRIMARY KEY,
     url_id INTEGER NOT NULL REFERENCES urls(id),
-    code_status INTEGER,
+    status_code INTEGER,
+    h1 TEXT,
+    title TEXT,
     description TEXT,
     created_at DATE DEFAULT CURRENT_DATE
 );
