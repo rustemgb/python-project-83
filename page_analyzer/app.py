a2fa390095b0dfdf7993f608a5f68f81adacd1c9
Rustem <rustemkago12345@gmail.com>
2025-11-19 10:43:54 +0300

    add flash

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,12 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask, flash, render_template, request
+from flask import Flask, flash, render_template, request, redirect, url_for
+from page_analyzer.url_validator import validate_url
+from page_analyzer.parser import parse_url
 
-from page_analyzer.data_base import save_url, get_db_connection
+
+from page_analyzer.data_base import get_db_connection, save_url, get_existing_urls
 
 load_dotenv()
 app = Flask(__name__)
@@ -18,6 +21,34 @@ def index():
 @app.route("/urls", methods=["POST"])
 def create_url():
     input_url = request.form.get("url")
+    error_message = validate_url(input_url)
+    if error_message:
+        flash(error_message, "error")
+        return render_template ("index.html", url=input_url), 422
+    base_input_domain = parse_url(input_url)
+    existing_urls = get_existing_urls()
+    if existing_urls:
+        for existing_url in existing_urls:
+            if base_input_domain == parse_url(existing_url[1]):
+                flash('Страница уже существует', 'error')
+                return redirect(url_for('url_detail', url_id=existing_url[0]))
     url_id = save_url(input_url)
     flash('Страница успешно добавлена', 'success')
-    return render_template('index.html', url_id=url_id)
+    return redirect(url_for('url_detail', url_id=url_id))
+
+
+@app.route("/urls", methods=["GET"])
+def all_urls():
+    sql = '''
+        SELECT urls.id, urls.name, urls.created_at,
+            url_checks.code_status,
+            url_checks.created_at AS last_check, 
+            url_checks.description
+        FROM urls
+        LEFT JOIN url_checks ON urls.id = url_checks.url_id
+    '''
+    with get_db_connection() as conn:
+        with conn.cursor() as curs:
+            curs.execute(sql)
+            urls = curs.fetchall()
+            return render_template("urls.html", urls=urls)
